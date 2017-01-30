81ec8eb4012fd3c95374c78ea347e2f42bb60b33
jojo <jonathan.rodriguez@roiback.com>
2017-01-30 17:02:40 +0100

    upgrade version

--- src/MasterWatcher.js
+++ src/MasterWatcher.js
@@ -118,7 +118,7 @@ export default class MasterWatcher {
                         stack: args.stack,
                         timestamp: util.isoNow(),
                         visitor: this.visitorWatcher.report(),
-                        version: '1.0.4'
+                        version: '1.0.7'
                     });
 
                     if (!options.force) {
@@ -168,7 +168,7 @@ export default class MasterWatcher {
             msg: error.message || 'unknown',
             stack: (error.stack || 'unknown').substr(0, 500),
             url: this.window.location,
-            v: '1.0.4',
+            v: '1.0.7',
             h: '9b37e9ac0b951d1cc4f7724bcd102d9edbc4a5d2',
             x: util.uuid()
         };
